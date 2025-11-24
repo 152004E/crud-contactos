81fe3eeb603b550181665e812d375e3abf47e407
Emerson Reyes <reyesemerson643@gmail.com>
2025-11-24 18:59:28 -0500

    fix: add SSL config for Supabase connection

--- backend/db/conexion.js
+++ backend/db/conexion.js
@@ -1,5 +1,4 @@
 import pkg from "pg";
-//sirve para las variables de entorno como el user la contraseña el puerto etc
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,9 +7,12 @@ const {Pool} = pkg;
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    password :process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    port : process.env.DB_PORT
-})
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 export default pool;
